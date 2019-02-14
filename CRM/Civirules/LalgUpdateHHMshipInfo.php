2bbe642eedf63ce7569d557266f0b8f49a3347da
System User <reflalgo@shell3.gridhost.co.uk>
2019-02-14 16:37:00 +0000

    Initial commit of the items required to set HH Membership Custom Fields.

---
+++ CRM/Civirules/LalgUpdateHHMshipInfo.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * CiviRules Action to set selected Membership Fields in Household Custom Fields
+ * for use in Webforms.
+ *
+ *********************************  NOTE  ********************************
+ * Assumes that CiviRule Conditions are set to ensure:
+ *   The Contact is a Household
+ */
+class CRM_Civirules_LalgUpdateHHMshipInfo extends CRM_Civirules_Action {
+
+	/**
+	 * Method to return the url for additional form processing for action
+	 * and return false if none is needed
+	 *
+	 * @param int $ruleActionId
+	 * @return bool
+	 * @access public
+	 */
+	public function getExtraDataInputUrl($ruleActionId) {
+	  return FALSE;
+	}
+
+	/**
+	 * Method processAction to execute the action
+	 *
+	 * @param CRM_Civirules_TriggerData_TriggerData $triggerData
+	 * @access public
+	 *
+	 */
+	public function processAction(CRM_Civirules_TriggerData_TriggerData $triggerData) {
+	  try {	
+		// Get Household Id
+		$contactId = $triggerData->getContactId();
+	    //dpm('Action called for Id: ' . $contactId);
+		
+		// Get the related Membership 
+		//dpm('Getting the related Household Membership');
+		$result = civicrm_api3('Membership', 'get', ['sequential' => 1, 'contact_id' => $contactId,]);
+		$membership = $result['values'][0];
+		//dpm($membership);
+		
+		// Format Membership Expiry Date
+		$endDate = DateTime::createFromFormat('Y-m-d', $membership['end_date']);
+		$endDate = $endDate->format('F jS, Y');
+		//dpm($endDate);
+		// Set End Date field
+		$result = civicrm_api3('CustomValue', 'create', [
+		  'entity_id' => $contactId, 
+		  'custom_Household_Fields:Membership_End_Date' => $endDate,
+		]);
+		
+		// Get Membership Type
+		$membershipType = $membership['membership_name'];
+		// Get Status
+		$result = civicrm_api3('MembershipStatus', 'get', [
+		  'sequential' => 1,
+		  'id' => $membership['status_id'],
+		]);
+		//dpm($result);
+		$status = $result['values'][0];
+		if(!$status['is_current_member']) {
+			$membershipType = $status['label'];
+		}
+		
+		// Set Membership Type custom field
+		$result = civicrm_api3('CustomValue', 'create', [
+		  'entity_id' => $contactId, 
+		  'custom_Household_Fields:Membership_Type' => $membershipType,
+		]);
+		
+	  } 
+	  catch (Exception $e) {
+		dpm('LalgUpdateHHMshipInfo  Caught exception: '.  $e->getMessage());
+	  }
+	}
+	
+	  
+}
--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -4,5 +4,12 @@ INSERT INTO civirule_action (name, label, class_name, is_active)
 VALUES("lalg_clear_print_flag", "LALG Clear Print Flag", "CRM_Civirules_LalgClearPrintFlag", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_autocomplete_payment", "LALG Autocomplete Payment", "CRM_Civirules_LalgAutocompletePayment", 1);
+INSERT INTO civirule_action (name, label, class_name, is_active) 
+VALUES("lalg_update_hh_mship_info", "LALG Update HH Mship Info", "CRM_Civirules_LalgUpdateHHMshipInfo", 1);
+
 INSERT INTO civirule_condition (name, label, class_name, is_active) 
 VALUES("lalg_email_card_conditions", "LALG Email Card Conditions", "CRM_Civirules_LalgEmailCardConditions", 1);
+INSERT INTO civirule_condition (name, label, class_name, is_active) 
+VALUES("lalg_print_flag_conditions", "LALG Print Flag Conditions", "CRM_Civirules_LalgPrintFlagConditions", 1);
+INSERT INTO civirule_condition (name, label, class_name, is_active) 
+VALUES("lalg_contact_is_hh_condition", "LALG Contact Is HH", "CRM_Civirules_LalgContactIsHHCondition", 1);
