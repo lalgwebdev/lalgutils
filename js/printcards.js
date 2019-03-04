b3ea6a3e02d4453dd0329eb30db2549a9cd0a24a
System User <reflalgo@shell3.gridhost.co.uk>
2019-03-04 12:17:26 +0000

    Improvements (I hope) to the JS on Print Cards Task page.

--- js/printcards.js
+++ js/printcards.js
@@ -1,11 +1,12 @@
 // Simplify the PDF printing page for Membership Cards
 CRM.$(function($) {
-  $('.crm-contact-task-pdf-form-block table:nth-of-type(1) input.crm-form-file').hide();
-  $('.crm-contact-task-pdf-form-block table:nth-of-type(1) tr:nth-of-type(2)').hide();
-  $('.crm-contact-task-pdf-form-block select#template').val('71').change();
+	jQuery(document).ready(function(){
+ 
+	  $('.crm-contact-task-pdf-form-block select#template').val('71').change();
+	  $('#_qf_LalgPrintCards_upload-bottom').val('Download and clear flags');
+	  $('#_qf_LalgPrintCards_submit_preview-bottom').val('Download');
 
-  setTimeout(() => $('.crm-contact-task-pdf-form-block > .crm-accordion-wrapper').hide(), 1000);
-
-  $('#_qf_LalgPrintCards_upload-bottom').val('Download and clear flags');
-  $('#_qf_LalgPrintCards_submit_preview-bottom').val('Download');
+	  $('.crm-contact-task-pdf-form-block table').hide();
+	  setTimeout(() => $('.crm-contact-task-pdf-form-block div.crm-accordion-wrapper').hide(), 1000);
+	});
 });
