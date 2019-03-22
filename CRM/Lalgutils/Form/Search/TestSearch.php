1ccdc96d354e52a3a404422172c6f974bd3f31ec
Tony <reflalgo@shell3.gridhost.co.uk>
2019-03-22 01:17:59 +0000

    New version of Delete Members Search, named TestSearch

---
+++ CRM/Lalgutils/Form/Search/TestSearch.php
@@ -0,0 +1,57 @@
+<?php
+use CRM_Lalgutils_ExtensionUtil as E;
+
+/**
+ ****************************  LALG Custom Search  ********************
+ * For use as start of the Delete Members workflow
+ * Extends 'Basic' Custom Search
+ */
+class CRM_Lalgutils_Form_Search_TestSearch extends CRM_Contact_Form_Search_Custom_Basic implements CRM_Contact_Form_Search_Interface {
+
+   /**
+   * Class constructor.
+   *
+   * @param array $formValues
+   */
+  public function __construct(&$formValues) {
+    parent::__construct($formValues);
+
+    $this->_columns = array(
+      '' => 'contact_type',
+      ts('Name') => 'sort_name',
+      ts('Address') => 'street_address',
+      ts('City') => 'city',
+      ts('Postcode') => 'postal_code',
+      ts('Email') => 'email',
+    );
+  }
+  
+  /**
+   * Build the Search Form
+   *   @param CRM_Core_Form $form
+   */
+  public function buildForm(&$form) {
+	parent::buildForm($form);
+    CRM_Utils_System::setTitle(E::ts('Contacts to Delete'));
+  }  
+  
+  /**
+   * Modify the tasklist
+   *
+   * Specify the tasks to be available.  The important bit is $key below.
+   */
+  public function buildTaskList($form) {
+    $tasks = parent::buildTaskList($form);
+	//dpm($tasks);
+    $newtasks = [];
+    foreach ($tasks as $key => $title) {
+      if ($title == 'LALG - Delete Members') {
+        $newTasks[$key] = $title;
+      }
+    }
+    return $newTasks;
+  }  
+
+}
+
+
