d8d4ec80bf0e4f0ea7733a312b683a160def1602
Tony <reflalgo@shell3.gridhost.co.uk>
2019-05-24 09:52:28 +0100

    Add Tidy Billing Email Action.  General Tidy

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -1,13 +1,11 @@
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_clear_email_flag", "LALG Clear Email Flag", "CRM_Civirules_LalgClearEmailFlag", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
-VALUES("lalg_autocomplete_payment", "LALG Autocomplete Payment", "CRM_Civirules_LalgAutocompletePayment", 1);
-INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_update_hh_mship_info", "LALG Update HH Mship Info", "CRM_Civirules_LalgUpdateHHMshipInfo", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_delete_dummy_email", "LALG Delete Dummy Emails", "CRM_Civirules_LalgDeleteDummyEmail", 1);
+INSERT INTO civirule_action (name, label, class_name, is_active) 
+VALUES("lalg_tidy_billing_email", "LALG Tidy Billing Email", "CRM_Civirules_LalgTidyBillingEmail", 1);
 
-INSERT INTO civirule_condition (name, label, class_name, is_active) 
-VALUES("lalg_email_card_conditions", "LALG Email Card Conditions", "CRM_Civirules_LalgEmailCardConditions", 1);
-INSERT INTO civirule_condition (name, label, class_name, is_active) 
-VALUES("lalg_contact_is_hh_condition", "LALG Contact Is HH", "CRM_Civirules_LalgContactIsHHCondition", 1);
+
+
