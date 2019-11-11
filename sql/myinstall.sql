3dcfb9f8dae1de934d8d9cc5ea9441e55d4ae168
lalgwebdev <webtech@lalg.org.uk>
2019-11-11 17:47:07 +0200

    Correct the install sql file for earlier commit

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -5,8 +5,6 @@ VALUES("lalg_update_hh_mship_info", "LALG Update HH Mship Info", "CRM_Civirules_
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_delete_dummy_email", "LALG Delete Dummy Emails", "CRM_Civirules_LalgDeleteDummyEmail", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
-VALUES("lalg_tidy_billing_email", "LALG Tidy Billing Email", "CRM_Civirules_LalgTidyBillingEmail", 1);
-INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_tidy_billing_address", "LALG Tidy Billing Address", "CRM_Civirules_LalgTidyBillingAddress", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_display_triggerdata", "LALG Display TrggerData", "CRM_Civirules_LalgDisplayTriggerData", 1);
