4fe26ddbd8baa17fcd0668387a32a24e06b1147c
lalgwebdev <webtech@lalg.org.uk>
2019-11-27 14:10:31 +0200

    Correct myinstall.sql for rule changes

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -1,11 +1,9 @@
 INSERT INTO civirule_action (name, label, class_name, is_active) 
-VALUES("lalg_clear_email_flag", "LALG Clear Email Flag", "CRM_Civirules_LalgClearEmailFlag", 1);
-INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_update_hh_mship_info", "LALG Update HH Mship Info", "CRM_Civirules_LalgUpdateHHMshipInfo", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_delete_dummy_email", "LALG Delete Dummy Emails", "CRM_Civirules_LalgDeleteDummyEmail", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_tidy_billing_address", "LALG Tidy Billing Address", "CRM_Civirules_LalgTidyBillingAddress", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
-VALUES("lalg_display_triggerdata", "LALG Display TrggerData", "CRM_Civirules_LalgDisplayTriggerData", 1);
+VALUES("lalg_display_triggerdata", "LALG Display TriggerData", "CRM_Civirules_LalgDisplayTriggerData", 1);
 
