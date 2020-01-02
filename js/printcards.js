109af111e30af7b7e3b0784e2a581fb542d4f9f8
lalgwebdev <webtech@lalg.org.uk>
2020-01-02 19:55:56 +0200

    Tweak JS code for PrintCards template loading.

--- js/printcards.js
+++ js/printcards.js
@@ -1,11 +1,23 @@
 // Simplify the PDF printing page for Membership Cards
 CRM.$(function($) {
 	jQuery(document).ready(function(){
-		setTimeout(function(){
-			$('.crm-contact-task-pdf-form-block select#template').val('71').change();
-			$('#_qf_LalgPrintCards_upload-bottom').val('Download and clear flags');
-			$('#_qf_LalgPrintCards_submit_preview-bottom').val('Download');
-		}, 1000);
+		
+		function checkBold (rep) {
+			console.log('Bold: ' + $('#cke_24').is(':visible') );
+			// Waits for the Bold button in the CKEditor to appear
+			if ($('#cke_24').is(':visible') ) {
+				setTimeout(function(){
+					$('.crm-contact-task-pdf-form-block select#template').val('71').change();
+					$('#_qf_LalgPrintCards_upload-bottom').val('Download and clear flags');
+					$('#_qf_LalgPrintCards_submit_preview-bottom').val('Download');
+				}, 500);				
+				return;
+			}
+			if (rep > 0) {setTimeout(() => checkBold(rep-1), 100);}
+		}
+		
+		checkBold(100);					// Limit of 10 seconds
+		
 //	  $('.crm-contact-task-pdf-form-block table').hide();
 
 		// function checkIframe (rep) {
