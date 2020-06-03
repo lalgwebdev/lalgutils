1e4de45a04b5f1f9bc2a76e3d55fecc32b1843be
lalgwebdev <webtech@lalg.org.uk>
2020-06-03 19:13:08 +0300

    Add CiviRule for 3 Month Membership Extension

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -6,4 +6,5 @@ INSERT INTO civirule_action (name, label, class_name, is_active)
 VALUES("lalg_tidy_billing_address", "LALG Tidy Billing Address", "CRM_Civirules_LalgTidyBillingAddress", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_display_triggerdata", "LALG Display TriggerData", "CRM_Civirules_LalgDisplayTriggerData", 1);
-
+INSERT INTO civirule_action (name, label, class_name, is_active) 
+VALUES("lalg_3_month_extension", "LALG 3 Month Extension", "CRM_Civirules_Lalg3MonthExtension", 1);
