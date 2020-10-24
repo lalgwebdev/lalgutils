385016364fa9fb6639b465ecbe50d841c07a601e
lalgwebdev <webtech@lalg.org.uk>
2020-10-24 16:47:54 +0300

    Tidy CiviRules action - Delete dummy emails.

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -1,8 +1,6 @@
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_update_hh_mship_info", "LALG Update HH Mship Info", "CRM_Civirules_LalgUpdateHHMshipInfo", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
-VALUES("lalg_delete_dummy_email", "LALG Delete Dummy Emails", "CRM_Civirules_LalgDeleteDummyEmail", 1);
-INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_tidy_billing_address", "LALG Tidy Billing Address", "CRM_Civirules_LalgTidyBillingAddress", 1);
 INSERT INTO civirule_action (name, label, class_name, is_active) 
 VALUES("lalg_display_triggerdata", "LALG Display TriggerData", "CRM_Civirules_LalgDisplayTriggerData", 1);
