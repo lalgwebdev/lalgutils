cae531b33e51806bfbca707bbd97c1f8dcce7161
lalgwebdev <webtech@lalg.org.uk>
2020-12-15 08:06:20 +0200

    Change PrintCards JS for new markup in CiviCRM 5.31

--- js/printcards.js
+++ js/printcards.js
@@ -3,13 +3,13 @@ CRM.$(function($) {
 	jQuery(document).ready(function(){
 		
 		function checkBold (rep) {
-			console.log('Bold: ' + $('#cke_24').is(':visible') );
+//			console.log('Bold: ' + $('#cke_24').is(':visible') );
 			// Waits for the Bold button in the CKEditor to appear
 			if ($('#cke_24').is(':visible') ) {
 				setTimeout(function(){
 					$('.crm-contact-task-pdf-form-block select#template').val('71').change();
-					$('#_qf_LalgPrintCards_upload-bottom').val('Download and clear flags');
-					$('#_qf_LalgPrintCards_submit_preview-bottom').val('Download');
+					$('#_qf_LalgPrintCards_upload-bottom').text('Download and clear flags');
+					$('#_qf_LalgPrintCards_submit_preview-bottom').text('Download');
 				}, 500);				
 				return;
 			}
