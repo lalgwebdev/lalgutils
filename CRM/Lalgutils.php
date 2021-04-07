15a704d1ee718de4a91ec554e0bf17269037d348
lalgwebdev <webtech@lalg.org.uk>
2021-04-07 11:28:06 +0300

    Update Print Cards search for changes to API3

--- CRM/Lalgutils.php
+++ CRM/Lalgutils.php
@@ -8,29 +8,26 @@ class CRM_Lalgutils {
    * Note - these are the ids of people, not the households.
    */
   public function clear_print_flag($cids) {
-	// Get the id of the Send_Membership_Documents custom Field.  
-    static $tagid;
-    if (!$tagid) {
-      $tagid = civicrm_api3('Tag', 'getvalue', [
-        'return' => "id",
-        'name' => "Print Card",
-      ]);
-      $tagid = CRM_Utils_Type::escape($tagid, 'Int');
-    }
-
     if (!is_array($cids)) {
       $cids = explode(",", $cids);
     }
-	
+
     foreach ($cids as $cid) {
 		// Set printfield off
-		$result = civicrm_api3('EntityTag', 'delete', [
-		  'tag_id' => $tagid,
+		// Get Id of join table entry
+		$entityTagId = civicrm_api3('EntityTag', 'getvalue', [
+		  'return' => "id",
+		  'entity_table' => "civicrm_contact",
+		  'tag_id' => "Print Card",
 		  'entity_id' => $cid,
+		]);
+		// Delete
+		$result = civicrm_api3('EntityTag', 'delete', [
+		  'id' => $entityTagId,
+		  'contact_id' => $cid,
 		]);
 		// Create Activity
 		$result = civicrm_api3('Activity', 'create', [
-//		  'source_contact_id' => "user_contact_id",
 		  'activity_type_id' => 55,				// "Print Membership Card",
 		  'status_id' => 2,						// "Completed",
 		  'target_id' => $cid,
