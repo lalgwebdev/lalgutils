54d12bc904e5a31c8b01467bf6040bad622e2757
lalgwebdev <webtech@lalg.org.uk>
2021-05-19 05:01:19 +0300

    Fix creating duplicate Actions on install

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -1,7 +1,19 @@
 INSERT INTO civirule_action (name, label, class_name, is_active) 
-VALUES("lalg_update_hh_mship_info", "LALG Update HH Mship Info", "CRM_Civirules_LalgUpdateHHMshipInfo", 1);
+SELECT * FROM (SELECT "lalg_update_hh_mship_info" as name, "LALG Update HH Mship Info" as label, "CRM_Civirules_LalgUpdateHHMshipInfo" as class_name, 1 as is_active) as temp
+WHERE NOT EXISTS (
+    SELECT name FROM civirule_action WHERE name = 'lalg_update_hh_mship_info'
+) LIMIT 1;
+
 INSERT INTO civirule_action (name, label, class_name, is_active) 
-VALUES("lalg_tidy_billing_address", "LALG Tidy Billing Address", "CRM_Civirules_LalgTidyBillingAddress", 1);
+SELECT * FROM (SELECT "lalg_tidy_billing_address" as name, "LALG Tidy Billing Address" as label, "CRM_Civirules_LalgTidyBillingAddress" as class_name, 1 as is_active) as temp
+WHERE NOT EXISTS (
+    SELECT name FROM civirule_action WHERE name = 'lalg_tidy_billing_address'
+) LIMIT 1;
+
 INSERT INTO civirule_action (name, label, class_name, is_active) 
-VALUES("lalg_display_triggerdata", "LALG Display TriggerData", "CRM_Civirules_LalgDisplayTriggerData", 1);
+SELECT * FROM (SELECT "lalg_display_triggerdata" as name, "LALG Display TriggerData" as label, "CRM_Civirules_LalgDisplayTriggerData" as class_name, 1 as is_active) as temp
+WHERE NOT EXISTS (
+    SELECT name FROM civirule_action WHERE name = 'lalg_display_triggerdata'
+) LIMIT 1;
+
 
