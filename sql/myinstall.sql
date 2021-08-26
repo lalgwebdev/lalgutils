61975f29f06263eb51ca42eef354ae561992978a
maynardsmith <tony.maynard-smith@ntlworld.com>
2021-08-26 18:14:30 +0300

    Update version to 0.3

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -11,6 +11,12 @@ WHERE NOT EXISTS (
 ) LIMIT 1;
 
 INSERT INTO civirule_action (name, label, class_name, is_active) 
+SELECT * FROM (SELECT "lalg_tidy_billing_email" as name, "LALG Tidy Billing Email" as label, "CRM_Civirules_LalgTidyBillingEmail" as class_name, 1 as is_active) as temp
+WHERE NOT EXISTS (
+    SELECT name FROM civirule_action WHERE name = 'lalg_tidy_billing_email'
+) LIMIT 1;
+
+INSERT INTO civirule_action (name, label, class_name, is_active) 
 SELECT * FROM (SELECT "lalg_display_triggerdata" as name, "LALG Display TriggerData" as label, "CRM_Civirules_LalgDisplayTriggerData" as class_name, 1 as is_active) as temp
 WHERE NOT EXISTS (
     SELECT name FROM civirule_action WHERE name = 'lalg_display_triggerdata'
