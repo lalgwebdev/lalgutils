69b2371b8f0f9ebf2bd02db0e4cced8f74a9a1fb
maynardsmith <tony.maynard-smith@ntlworld.com>
2021-11-21 19:31:40 +0200

    Add Tokens for reworked Postal Reminder Template.  Version up to 0.6

--- CRM/Civitokens/Tokens.php
+++ CRM/Civitokens/Tokens.php
@@ -12,12 +12,12 @@ class CRM_Civitokens_Tokens {
 * Params - Array of tokens to be declared
 */
 	public static function civicrm_tokens(&$tokens) {
-		$tokens['paylater'] = array(
-			'paylater.membership_fee' => 'Pay Later - Membership Fee',
+		$tokens['reminder'] = array(
+			'reminder.latest_subject' => 'Latest Scheduled Reminder Subject',
 		);
-		$tokens['activity'] = array(
-			'activity.scheduled_reminder' => 'Activity - Latest Scheduled Reminder',
-		);
+		$tokens['template'] = array(
+			'template.how_to_pay' => 'Template - How to Pay',
+		);		
 	}
 	
 /** 
@@ -30,57 +30,57 @@ class CRM_Civitokens_Tokens {
 	public static function civicrm_tokenValues(&$values, $cids, $job = null, $tokens = array(), $context = null) {
 //dpm($cids);
 //dpm($tokens);
-		// Pay Later tokens if these are required
-		if (!empty($tokens['paylater'])) {  
-		// Cycle through the Contacts list
-			foreach ($cids as $cid) {
-				// Get Pending Contributions
-					
-				$result = civicrm_api3('Contribution', 'get', [
-				  'sequential' => 1,
-				  'contact_id' => $cid,
-				  'contribution_status_id' => 2, 	// Pending
-				]);
-				if (empty($result['values'])) {
-					CRM_Core_Session::setStatus('Contact Reference ' . $cid . ' has no Pending Contribution. Skipped.',
-												'Warning');
-					continue;						// Move to next Contact
-				}
-				// Get last result if more than one
-				foreach ($result['values'] as $contrib) {
-					$contribId = $contrib['contribution_id'];
-				// Set return value
-					$values[$cid]['paylater.membership_fee'] = $contrib['total_amount'];
-				}
-	//dpm($result);				
-			}
-		}
 		
 		// Activity tokens if these are required
-		if (!empty($tokens['activity'])) {  
+		if (!empty($tokens['reminder'])) {  
 		// Cycle through the Contacts list
 			foreach ($cids as $cid) {
-				// Get associated Activities
-				$result = civicrm_api3('Activity', 'get', [
-				  'sequential' => 1,
-				  'target_contact_id' => $cid,
-				  'activity_type_id' => "Print Postal Reminder",
-				  'status_id' => "Scheduled",
+				// Get latest Activities
+				$result = civicrm_api4('Activity', 'get', [
+				  'join' => [
+					['ActivityContact AS activity_contact', 'LEFT', ['id', '=', 'activity_contact.activity_id']],
+				  ],
+				  'where' => [
+					['activity_type_id:name', '=', 'Print Postal Reminder'], 
+					['activity_contact.contact_id', '=', $cid], 
+					['activity_contact.record_type_id', '=', 3], 	// Contact is Activity Target
+					['status_id:name', '=', 'Scheduled'],			// Scheduled
+				  ],
+				  'orderBy' => [ 'created_date' => 'DESC' ],		// Most recent First
+				  'limit' => 1,										// Get only the latest
 				]);
-//dpm($result);
-				if (empty($result['values'])) {
-					CRM_Core_Session::setStatus('Contact Reference ' . $cid . ' has no Scheduled Reminder. Skipped.',
-												'Warning');
+//dpm($result[0]);
+				if (empty($result[0])) {
+					CRM_Core_Session::setStatus('Contact Reference ' . $cid . ' has no Scheduled Reminder. Skipped.', 'Warning');
 					continue;						// Move to next Contact
 				}
-				// Get last result if more than one
-				foreach ($result['values'] as $activity) {
 				// Set return value
-					$values[$cid]['activity.scheduled_reminder'] = $activity['subject'];
-				}
-//dpm($values);
+				$values[$cid]['reminder.latest_subject'] = $result[0]['subject'];
 			}
+//dpm($values);
 		}
 		
+		// Template tokens if these are required
+		if (!empty($tokens['template'])) {  
+			STATIC $how_to_pay;									// Persistent variable
+			// Get the How to Pay template
+			if (!$how_to_pay) {
+				$result = civicrm_api4('MessageTemplate', 'get', [
+				  'select' => [
+					'msg_html',
+				  ],
+				  'where' => [
+					['msg_title', '=', 'Sys - How to Pay'],
+				  ],
+				]);
+//dpm($result[0]);
+				$how_to_pay = $result[0]['msg_html'];
+			}
+			
+			// Cycle through the Contacts list
+			foreach ($cids as $cid) {
+				$values[$cid]['template.how_to_pay'] = $how_to_pay;
+			}	
+		}
 	}	
 }
