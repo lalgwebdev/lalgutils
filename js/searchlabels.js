d9f2000e2317eb19d7df3cd3f2be5119d6221267
maynardsmith <tony.maynard-smith@ntlworld.com>
2022-07-03 19:23:18 +0300

    Add Export Data to LALG Print Labels.  First cut.

--- js/searchlabels.js
+++ js/searchlabels.js
@@ -3,6 +3,12 @@ jQuery(document).ready(function(){
 // Remove all the Actions on the Search Labels page, except LALG Print Labels 
 
 	//console.log('Clearing Actions');
-	jQuery('select#task option[value!="112"]').not(':first-child').remove();
+//	jQuery('select#task option[value!="112"]').not(':first-child').remove();
+	jQuery('select#task option').not(':first-child').each(function(){
+      val = jQuery(this).attr("value");
+	  if (val != 8 && val != 112) {
+		  jQuery(this).remove();
+	  }
+    });
 	
 });
